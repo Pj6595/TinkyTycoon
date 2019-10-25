d93922591b131efe01c356c38ef8d6eb784bff42
Pablo <pjurad01@ucm.es>
2019-10-25 18:05:56 +0200

    Arreglado merge conflict con MainMenu

--- docs/scripts/Planet.js
+++ docs/scripts/Planet.js
@@ -1,10 +1,12 @@
 import Player from './Player.js'
+import Crater from './Crater.js'
 
 export default class Planet extends Phaser.Scene{
     constructor(){
         super({key: 'Planet'})
     }
     create(){
-		this.player = new Player(this, 200, 300);
+        this.player = new Player(this, 200, 300);
+        this.crater1 = new Crater(this, 100, 100,6);
 	}
 }
