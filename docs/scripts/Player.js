41d451e467dabe50e682c70d1d0671fb2c7b7ec4
Esteban <estrestrepo+github@gmail.com>
2019-11-04 18:46:20 +0100

    Movement with WASD

--- docs/scripts/Player.js
+++ docs/scripts/Player.js
@@ -7,31 +7,37 @@ export default class Player extends Phaser.GameObjects.Sprite{
 		//Physics
 		this.scene.physics.add.existing(this);
 		this.body.setCollideWorldBounds();
-		//Character control
-		this.cursors = this.scene.input.keyboard.createCursorKeys();
-		this.speed = 100;
-		this.scale = 0.1;
 
 		this.inventory = new Inventory(tinkyValue, inventoryCapacity);
 
 		this.toolTier = 1;
+
+		//Character control
+		this.cursors = this.scene.input.keyboard.createCursorKeys();
+		this.speed = 100;
+		this.scale = 0.1;
+		this.w = scene.input.keyboard.addKey('W');
+		this.s = scene.input.keyboard.addKey('S');
+		this.a = scene.input.keyboard.addKey('A');
+		this.d = scene.input.keyboard.addKey('D');
+		
 	}
 
 	preUpdate(){
-		if(this.cursors.down.isDown){
+		if(this.cursors.down.isDown || this.s.isDown){
 			this.body.setVelocityY(this.speed);
 		}
-		else if(this.cursors.up.isDown){
+		else if(this.cursors.up.isDown || this.w.isDown){
 			this.body.setVelocityY(-this.speed);
 		}else{
 			this.body.setVelocityY(0);
 		}
 			
 
-		if(this.cursors.left.isDown){
+		if(this.cursors.left.isDown || this.a.isDown){
 			this.body.setVelocityX(-this.speed);
 		}
-		else if(this.cursors.right.isDown){
+		else if(this.cursors.right.isDown || this.d.isDown){
 			this.body.setVelocityX(this.speed);
 		}else{
 			this.body.setVelocityX(0);
